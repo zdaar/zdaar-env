8558179db9d3f7122ad02eeb5e2c1d7bc2871f94
zdaar <zdaryl@gmail.com>
2023-09-16 12:51:29 +0200

    add git aliases and vim_setup

---
+++ git_setup.sh
@@ -0,0 +1,3 @@
+git config --global alias.s "status -s"
+git config --global alias.cm "commit -m"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit"
---
+++ vim_setup.sh
@@ -0,0 +1,1 @@
+echo "set hlsearch" >> ~/.vimrc
